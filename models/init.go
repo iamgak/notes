ee2a9ecdf48f509c74c4aa33db95531db51a2295
iamgak <gak.gufrankhan.gak@gmail.com>
2025-02-15 01:44:36 +0530

    structure Construct

--- models/init.go
+++ models/init.go
@@ -16,7 +16,6 @@ func Constructor(db *sql.DB, redis *redis.Client) *Init {
 	return &Init{
 		Todo:  ToDoModel{db: db, redis: redis},
 		Users: UserModel{db: db, redis: redis},
-		Gpt:   Prompt(),
 		// Review: ReviewModel{db: db, redis: rd},
 	}
 }
